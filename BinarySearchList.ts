17c3af256ec0825146b6612085ea64c912fb42ea
Raeliss <israelfargasasquith@gmail.com>
2024-09-19 18:02:00 +0200

    first video

--- BinarySearchList.ts
+++ BinarySearchList.ts
@@ -1,3 +1,7 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let bottom = 0;
+    let top = haystack.length;
+    let mid = Math.floor(haystack.length / 2);
+
 
 }

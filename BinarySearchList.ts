4118dd827ab0184b5038a3d6d6dd5e0bbcf46665
Raeliss <israelfargasasquith@gmail.com>
2024-10-20 22:28:47 +0200

    3 more but I have to config tsconf for yarn

--- BinarySearchList.ts
+++ BinarySearchList.ts
@@ -1,7 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
     let bottom = 0;
     let top = haystack.length;
-    let mid = Math.floor(haystack.length / 2);
-
-
+    let mid;
+    let value;
+    while (top > bottom) {
+        mid = Math.floor(bottom + (top - bottom) / 2);
+        value = haystack[mid];
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            bottom = mid + 1;
+        } else {
+            top = mid;
+        }
+    }
+    return false;
 }
--- TwoCrystalBalls.ts
+++ TwoCrystalBalls.ts
@@ -1,3 +1,42 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jump = Math.floor(Math.sqrt(breaks.length));
+    let balls = 2;
+    let i = 0;
+    while (i < breaks.length && balls == 2) {
+        if (balls == 2) {
+            if (breaks[i]) {
+                i -= jump;
+                balls--;
+            } else {
+                i += jump;
+            }
+        } else if (balls == 1) {
+            if (breaks[i]) {
+                i--;
+                balls--;
+            } else {
+                i++;
+            }
+        }
+    }
+    return i;
+
+    /*
+    const jmp = Math.floor(Math.sqrt(breaks.length));
+    let i = jmp;
+    for(;i<breaks.length; i+=jmp){
+        if(breaks[i]){
+        break;
+        }
+    }
+        i-=jmp;
+        for(let j = 0;j<=jmp && i< breaks.lenght; j++,i++){
+            if(breaks[i]){
+                return i;
+            }
+        }
+            return-1;
+    */
+
 
 }
